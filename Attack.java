0c86fc7bf429541fae772d57397b1981049dffc1
TylerBlankenbiller <33728690+TylerBlankenbiller@users.noreply.github.com>
2020-11-03 15:31:34 -0500

    Add files via upload

--- Attack.java
+++ Attack.java
@@ -1,1 +1,54 @@
 //Attack Class
+public class Attack()
+{
+    private String Name;
+    private int[] Damage;
+    private int[] Magnitude;
+    private String[] Target;//User, UserOffense, UserDefense, Opp...
+    
+    /*
+        Constructor
+        AttackName - Name of Attack Object to create
+        Creates specific Attack Object based on string arugment
+    */
+    public Attack(String AttackName){
+        if(AttackName == null){
+            System.out.println('Please Specify an Attack');
+            return null;
+        }
+        Name = AttackName;
+        if(AttackName.equalsIgnoreCase('Double-Edge'){
+            setStats([120, 40], [0, 0], ['Opponent', 'User']);
+        }
+        else if(AttackName.equalsIgnoreCase('Syntgesis'){
+            setStats([-50], [0], ['User']);
+        }
+        else if(AttackName.equalsIgnoreCase('Seed Bomb'){
+            setStats([80], [0], ['Opponent']);
+        }
+        else if(AttackName.equalsIgnoreCase('Growth'){
+            setStats([0], [1], ['UserOffense']);
+        }
+        else if(AttackName.equalsIgnoreCase('Fire Blitz'){
+            setStats([120, 40], [0, 0], ['Opponent', 'User']);
+        }
+        else if(AttackName.equalsIgnoreCase('Slash'){
+            setStats([70], [0], ['Opponent']);
+        }
+        else if(AttackName.equalsIgnoreCase('Flamethrower'){
+            setStats([90, 0], [0, -1], ['Opponent', 'OpponentOffense']);
+        }
+        else if(AttackName.equalsIgnoreCase('Fire Breath'){
+            setStats([60, 0], [0, -1], ['Opponent', 'OpponentOffense']);
+        }
+        else{
+            System.out.println("Sorry we don't support that Move");
+            return null;
+        }
+    }
+    
+    void setStats(int[] damage, int[] magnitude, String[] target){
+        
+    }
+    
+}
--- Pokemon.java
+++ Pokemon.java
@@ -1,12 +1,17 @@
 //Pokemon Factory
+import java.lang.Math;
 
 public class Pokemon()
 {
-    int HP;
-    float OffenseStatus;
-    float DefenseStatus;
-    String Type;
-    Attack Attacks[];
+    private int HP;
+    private float OffenseStatus;
+    private float DefenseStatus;
+    private float[] OffenseList = new float[9];
+    private float[] DefenseList = new float[9];
+    private int OffensePointer;
+    private int DefensePointer;
+    private String Type;
+    protected Attack[] Attacks = new Attack[4];
     
     /*
         Constructor
@@ -19,20 +24,68 @@ public class Pokemon()
             return null;
         }
         if(PokemonName.equalsIgnoreCase('Venusaur'){
-            return new Venusaur();
+            String[] Attacks = {''}
+            setStats(50, 6, 6, 'Grass', Attacks);
         }
         else if(PokemonName.equalsIgnoreCase('Charizard'){
-            return new Charizard();
+            String[] Attacks = {''}
+            setStats(50, 7, 5, 'Fire', Attacks);
         }
         else if(PokemonName.equalsIgnoreCase('Blastoise'){
-            return new Blastoise();
+            String[] Attacks = {''}
+            setStats(50, 5, 7, 'Water', Attacks);
+        }
+        else if(PokemonName.equalsIgnoreCase('Butterfree'){
+            String[] Attacks = {''}
+            setStats(40, 6, 5, 'Bug', Attacks);
+        }
+        else if(PokemonName.equalsIgnoreCase('Beedrill'){
+            String[] Attacks = {''}
+            setStats(40, 6, 5, 'Bug', Attacks);
+        }
+        else if(PokemonName.equalsIgnoreCase('Pidgeot'){
+            String[] Attacks = {''}
+            setStats(50, 5, 5, 'Flying', Attacks);
+        }
+        else if(PokemonName.equalsIgnoreCase('Raticate'){
+            String[] Attacks = {''}
+            setStats(40, 5, 5, 'Normal', Attacks);
+        }
+        else if(PokemonName.equalsIgnoreCase('Fearow'){
+            String[] Attacks = {''}
+            setStats(40, 6, 4, 'Flying', Attacks);
+        }
+        else if(PokemonName.equalsIgnoreCase('Arbok'){
+            String[] Attacks = {''}
+            setStats(40, 6, 5, 'Poison', Attacks);
         }
         else if(PokemonName.equalsIgnoreCase('Pikachu'){
-            return new Pikachu();
+            String[] attacks = {''}
+            setStats(40, 6, 5, 'Electric', Attacks);
         }
         else{
             System.out.println("Sorry we don't support that Pokemon");
             return null;
+        }
+    }
+    
+    void SetStats(int myHP, float myOffenseStatus, float myDefenseStatus, String myType, String myAttacks[]){
+        HP = myHP;
+        OffenseStatus = myOffenseStatus;
+        DefenseStatus = myDefenseStatus;
+        for(int i = 0; i < 4; i++){
+            OffenseList[i] = myOffenseStatus * (0.4 + (i * 0.15));
+            DefenseList[i] = myDefenseStatus * (0.4 + (i * 0.15));
+        }
+        for(int i = 5; i < 9; i++){
+            OffenseList[i] = myOffenseStatus * (1 + ((i-4)*0.15));
+            DefenseList[i] = myDefenseStatus * (1 + ((i-4)*0.15));
+        }
+        OffensePointer = 4;
+        DefensePointer = 4;
+        Type = myType;
+        for(int i = 0; i< myAttacks.length; i++){
+            Attacks[i] = Attack(myAttacks[i]);
         }
     }
     
@@ -43,8 +96,33 @@ public class Pokemon()
         return true;
     }
     
-    void setOffensiveStatus(float statusChange){
-        OffenseStatus
+    //Set Functions
+    void SetOffenseStatus(int statusChange){
+        OffensePointer += statusChange;
+        OffenseStatus = OffenseList[OffensePointer];
+    }
+    
+    void SetDefenseStatus(int statusChange){
+        DefensePointer += statusChange;
+        DefenseStatus = DefenseList[DefensePointer];
+    }
+    
+    int SetHP(int damage, float offense){
+        int applyDamage = Math.round((offense * damage) / DefenseStatus);
+        HP -= applyDamage;
+        return applyDamage;
+    }
+    
+    //Get Functions
+    float GetOffenseStatus(){
+        return OffenseStatus;
+    }
+    float GetDefenseStatus(){
+        return DefenseStatus;
+    }
+    int GetHP(){
+        return HP;
     }
     
 }
+
